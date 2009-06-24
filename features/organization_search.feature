af3cf471ce19a58b326a88b369ec3388f887d829
Sinclair Bain <sinclair.bain@gmail.com>
2009-06-24 10:15:48 -0400

    added the organization search feature

---
+++ features/organization_search.feature
@@ -0,0 +1,11 @@
+Feature: Search for an Organization
+  In order to identify an organization 
+  As an software programmer
+  I want to be able to search for an Organization
+
+  Scenario: Successfully search for an Organization by name
+    Given that I have registered and received a MapLight api key from maplight.org
+    And that the MapLight module has been included
+    And that my MapLight api key has been set
+    When searching for all organizations with Glasgow in their name
+    Then the result should contain at least one Organization
---
+++ features/step_definitions/organization_search_steps.rb
@@ -0,0 +1,25 @@
+
+# encoding: utf-8
+require 'spec/expectations'
+#$:.unshift(File.dirname(__FILE__) + '/../lib') # This line is not needed in your own project
+require 'cucumber/formatter/unicode'
+require File.join( File.dirname(__FILE__), '..', '..', 'lib', 'maplight' )
+
+Given /that I have registered and received a MapLight api key from maplight.org/ do
+end
+
+Given /^that my MapLight api key has been set$/ do
+  MapLight.api_key= 'caee16151a22197259b754cdb09b18ac'
+end
+
+Given /^that the MapLight module has been included$/ do
+  include MapLight
+end
+
+When /^searching for all organizations with (.*) in their name$/ do |name|
+  @organizations = MapLight::Organization.search(name)
+end
+
+Then /^the result should contain at least one Organization$/ do
+  @organizations.should_not be_empty
+end
